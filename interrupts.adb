7c2dec77b756dbaf5f8d0efecbceac8e4034df77
ytomino <demoonlit@panathenaia.halfmoon.jp>
2016-06-18 20:43:43 +0900

    Rename the file names for consistency

---
+++ interrupts.adb
@@ -0,0 +1,26 @@
+with Ada.Interrupts.Names;
+with Ada.Text_IO; use Ada.Text_IO;
+procedure interrupts is
+	Break : Boolean := False;
+	pragma Atomic (Break);
+	protected Handlers is
+		procedure Handle_Break;
+--		pragma Interrupt_Handler (Handle_Break);
+	end Handlers;
+	protected body Handlers is
+		procedure Handle_Break is
+		begin
+			Put_Line (Standard_Error, "SIGINT");
+			Break := True;
+		end Handle_Break;
+	end Handlers;
+begin
+	Ada.Interrupts.Unchecked_Attach_Handler (Handlers.Handle_Break'Unrestricted_Access, Ada.Interrupts.Names.SIGINT);
+	Put_Line ("press ^C");
+	for I in 1 .. 10 loop
+		delay 1.0;
+		Put('*');
+		exit when Break;
+	end loop;
+	pragma Debug (Ada.Debug.Put ("OK"));
+end interrupts;
---
+++ maps_character_mapping.adb
@@ -0,0 +1,25 @@
+with Ada.Strings.Fixed;
+with Ada.Strings.Maps;
+procedure maps_character_mapping is
+	subtype C is Character;
+	X : Ada.Strings.Maps.Character_Mapping;
+	function M (Item : Wide_Wide_Character) return Wide_Wide_Character is
+	begin
+		case Item is
+			when 'A' => return 'B';
+			when 'B' => return 'A';
+			when others => return Item;
+		end case;
+	end M;
+	AIU : constant String := (
+		C'Val (16#e3#), C'Val (16#81#), C'Val (16#82#),
+		C'Val (16#e3#), C'Val (16#81#), C'Val (16#84#),
+		C'Val (16#e3#), C'Val (16#81#), C'Val (16#86#));
+begin
+	X := Ada.Strings.Maps.To_Mapping ("ABC", "ABC");
+	pragma Assert (Ada.Strings.Maps.Is_Identity (X));
+	X := Ada.Strings.Maps.To_Mapping ("ABC", AIU);
+	pragma Assert (Ada.Strings.Fixed.Translate ("ABCDE", X) = AIU & "DE");
+	pragma Assert (Ada.Strings.Fixed.Translate ("ABC", M'Access) = "BAC");
+	pragma Debug (Ada.Debug.Put ("OK"));
+end maps_character_mapping;
