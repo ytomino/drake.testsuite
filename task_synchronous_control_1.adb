e75d33adf87a1d4c9a54233d8dd998878a134dc3
ytomino <demoonlit@panathenaia.halfmoon.jp>
2016-07-01 02:32:35 +0900

    Improve the test cases for Ada.Synchronous_Task_Control

--- task_synchronous_control_1.adb
+++ task_synchronous_control_1.adb
@@ -2,10 +2,12 @@
 with Ada.Real_Time;
 with Ada.Synchronous_Task_Control;
 with Ada.Synchronous_Task_Control.EDF;
+with System.Synchronous_Control;
+with System.Tasks;
 procedure task_synchronous_control_1 is
-begin
-	-- Synchronous_Task_Control on single task
-	declare
+	use type System.Synchronous_Control.Yield_Handler;
+	
+	procedure Check_Single_Task is
 		Event : Ada.Synchronous_Task_Control.Suspension_Object;
 		State : Boolean;
 	begin
@@ -14,6 +16,7 @@ begin
 			Ada.Real_Time.To_Time_Span (0.0),
 			State);
 		pragma Assert (not State); -- initial state should be false
+		pragma Assert (not Ada.Synchronous_Task_Control.Current_State (Event));
 		Ada.Synchronous_Task_Control.Set_True (Event);
 		Ada.Synchronous_Task_Control.Suspend_Until_True (Event);
 		Ada.Synchronous_Task_Control.EDF.Suspend_Until_True_And_Set_Deadline (
@@ -21,37 +24,61 @@ begin
 			Ada.Real_Time.To_Time_Span (0.0),
 			State);
 		pragma Assert (State);
+		pragma Assert (Ada.Synchronous_Task_Control.Current_State (Event));
 		Ada.Synchronous_Task_Control.Set_False (Event);
 		Ada.Synchronous_Task_Control.EDF.Suspend_Until_True_And_Set_Deadline (
 			Event,
 			Ada.Real_Time.To_Time_Span (0.0),
 			State);
 		pragma Assert (not State);
-	end;
-	-- Synchronous_Task_Control
-	declare
-		ev : Ada.Synchronous_Task_Control.Suspension_Object;
+		pragma Assert (not Ada.Synchronous_Task_Control.Current_State (Event));
+	end Check_Single_Task;
+	
+	procedure Check_Multi_Task is
+		Event : Ada.Synchronous_Task_Control.Suspension_Object;
+		procedure Process (Param : System.Address) is
+			pragma Unreferenced (Param);
+		begin
+			delay 0.1;
+			Ada.Synchronous_Task_Control.Set_True (Event);
+		end Process;
+		Id : System.Tasks.Task_Id;
 		State : Boolean;
+		Aborted : Boolean;
 	begin
---		pragma Assert (not Ada.Synchronous_Task_Control.Current_State (ev));
-		Ada.Synchronous_Task_Control.Set_True (ev);
-		pragma Assert (Ada.Synchronous_Task_Control.Current_State (ev));
-		Ada.Synchronous_Task_Control.Suspend_Until_True (ev);
-		pragma Assert (Ada.Synchronous_Task_Control.Current_State (ev));
+		System.Tasks.Create (Id, System.Null_Address, Process'Access);
 		Ada.Synchronous_Task_Control.EDF.Suspend_Until_True_And_Set_Deadline (
-			ev,
-			Ada.Real_Time.To_Time_Span (1.0),
+			Event,
+			Ada.Real_Time.To_Time_Span (0.0),
+			State);
+		pragma Assert (not State);
+		Ada.Synchronous_Task_Control.Suspend_Until_True (Event);
+		Ada.Synchronous_Task_Control.EDF.Suspend_Until_True_And_Set_Deadline (
+			Event,
+			Ada.Real_Time.To_Time_Span (0.0),
 			State);
 		pragma Assert (State);
-		pragma Assert (Ada.Synchronous_Task_Control.Current_State (ev));
-		Ada.Synchronous_Task_Control.Set_False (ev);
-		pragma Assert (not Ada.Synchronous_Task_Control.Current_State (ev));
-		Ada.Synchronous_Task_Control.EDF.Suspend_Until_True_And_Set_Deadline (
-			ev,
-			Ada.Real_Time.To_Time_Span (1.0),
-			State); -- it may be timeout
-		pragma Assert (not State);
-		pragma Assert (not Ada.Synchronous_Task_Control.Current_State (ev));
-	end;
+		System.Tasks.Wait (Id, Aborted => Aborted);
+		pragma Assert (not Aborted);
+	end Check_Multi_Task;
+	
+begin
+	-- on single-task runtime
+	pragma Assert (
+		System.Synchronous_Control.Yield_Hook =
+		System.Synchronous_Control.Nop'Access);
+	Check_Single_Task;
+	pragma Assert (
+		System.Synchronous_Control.Yield_Hook =
+		System.Synchronous_Control.Nop'Access);
+	-- on multi-task runtime
+	Check_Multi_Task;
+	pragma Assert (
+		System.Synchronous_Control.Yield_Hook /=
+		System.Synchronous_Control.Nop'Access);
+	Check_Single_Task;
+	pragma Assert (
+		System.Synchronous_Control.Yield_Hook /=
+		System.Synchronous_Control.Nop'Access);
 	pragma Debug (Ada.Debug.Put ("OK"));
 end task_synchronous_control_1;
--- task_synchronous_control_abort.adb
+++ task_synchronous_control_abort.adb
@@ -1,27 +1,49 @@
 -- { dg-do run }
-with Ada.Synchronous_Task_Control;
-with System.Native_Tasks.Yield;
+with Ada.Real_Time;
+with Ada.Synchronous_Task_Control.EDF;
 procedure task_synchronous_control_abort is
 begin
-	Ada.Debug.Put ("**** break Suspend_Until_True ****");
+	--  abort Suspend_Until_True
 	declare
 		ev : Ada.Synchronous_Task_Control.Suspension_Object;
+		T1_State : Positive := 1;
 		task T1;
 		task body T1 is
 		begin
-			-- force switching to environment task
-			for I in 1 .. 500 loop
-				System.Native_Tasks.Yield;
-			end loop;
-			Ada.Debug.Put ("before wait in task");
+			T1_State := 2;
 			Ada.Synchronous_Task_Control.Suspend_Until_True (ev);
-			Ada.Debug.Put ("after wait in task");
+			T1_State := 3;
 			raise Program_Error; -- it does not come here
 		end T1;
 	begin
-		Ada.Debug.Put ("before abort");
+		delay 0.1;
+		pragma Assert (T1_State = 2);
 		abort T1;
-		Ada.Debug.Put ("after abort");
+		delay 0.1;
+		pragma Assert (T1'Terminated);
+		pragma Assert (T1_State = 2);
+	end;
+	--  abort Suspend_Until_True_And_Set_Deadline
+	declare
+		ev : Ada.Synchronous_Task_Control.Suspension_Object;
+		T2_State : Positive := 1;
+		task T2;
+		task body T2 is
+		begin
+			T2_State := 2;
+			Ada.Synchronous_Task_Control.EDF.Suspend_Until_True_And_Set_Deadline (
+				ev,
+				Ada.Real_Time.To_Time_Span (1.0));
+			T2_State := 3;
+			raise Program_Error; -- it does not come here
+		end T2;
+	begin
+		delay 0.1;
+		pragma Assert (T2_State = 2);
+		abort T2;
+		delay 0.1;
+		pragma Assert (T2'Terminated);
+		pragma Assert (T2_State = 2);
 	end;
 	pragma Debug (Ada.Debug.Put ("OK"));
 end task_synchronous_control_abort;
