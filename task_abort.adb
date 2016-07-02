7770c33148e7842f4bb45e01260da1bf09286a07
ytomino <demoonlit@panathenaia.halfmoon.jp>
2016-07-03 06:02:23 +0900

    Insert a test for the combination of abort and any exception from Finalize

--- task_abort.adb
+++ task_abort.adb
@@ -1,6 +1,7 @@
 -- { dg-do run }
 with Ada.Calendar;
 with Ada.Dispatching;
+with Ada.Finalization;
 with System.Tasks;
 procedure task_abort is
 	use type Ada.Calendar.Time;
@@ -113,6 +114,60 @@ begin
 		-- after abort
 		pragma Assert (T4_State = 5);
 		pragma Assert (T4_Child_State = 4);
+	end;
+	-- Triggered_By_Abort
+	declare
+		Some_Error : exception;
+		package Mocks is
+			type C1 is
+				limited new Ada.Finalization.Limited_Controlled with null record;
+			overriding procedure Finalize (Object : in out C1);
+		end Mocks;
+		package body Mocks is
+			overriding procedure Finalize (Object : in out C1) is
+				pragma Unreferenced (Object);
+			begin
+				raise Some_Error; -- hidden by Program_Error
+			end Finalize;
+		end Mocks;
+		T5_State : Positive := 1;
+		task T5;
+		task body T5 is
+		begin
+			begin
+				declare
+					Object : Mocks.C1;
+					pragma Unreferenced (Object);
+				begin
+					null;
+				end;
+				pragma Assert (False); -- it does not come here
+			exception
+				when Program_Error => -- hides Some_Error
+					T5_State := 2;
+			end;
+			pragma Assert (T5_State = 2);
+			begin
+				declare
+					Object : Mocks.C1;
+					pragma Unreferenced (Object);
+				begin
+					T5_State := 3;
+					delay 999.9;
+					T5_State := 4;
+					raise Program_Error; -- it does not come here
+				end;
+			exception
+				when Standard'Abort_Signal =>
+					-- Abort_Signal should not be hidden by any other exceptions.
+					System.Tasks.When_Abort_Signal;
+					T5_State := 5; -- aborted in task
+			end;
+		end T5;
+	begin
+		abort T5;
+		delay 0.1;
+		pragma Assert (T5_State = 5);
 	end;
 	pragma Debug (Ada.Debug.Put ("OK"));
 end task_abort;
