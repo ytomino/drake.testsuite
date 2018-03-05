0ea9b3aa7f9c35dd8abb13d2b6f95ff8ca33d32a
ytomino <demoonlit@panathenaia.halfmoon.jp>
2018-03-05 21:31:49 +0900

    Try Ada.Directories.Replace_File for two cases Target is existing or not

--- directories_replace_file.adb
+++ directories_replace_file.adb
@@ -7,35 +7,41 @@ procedure directories_replace_file is
 begin
 	Ada.Debug.Put (S);
 	Ada.Debug.Put (T);
-	declare
-		use ada.text_io;
-		file : file_type;
-	begin
-		create (file, Name => S);
-		put_line (file, "SOURCE");
-		close (file);
-		create (file, Name => T);
-		put_line (file, "TARGET");
-		close (file);
-	end;
-	ada.directories.replace_file (Source_Name => S, Target_Name => T);
-	pragma Assert (Ada.Directories.Exists (T));
-	pragma Assert (not Ada.Directories.Exists (S));
-	declare
-		use ada.text_io;
-		file : file_type;
-	begin
-		open (file, In_File, Name => T);
+	for Overwrite in Boolean loop
 		declare
-			Line : constant String := Get_Line (file);
+			use ada.text_io;
+			file : file_type;
 		begin
-			pragma Assert (Line = "SOURCE");
-			null;
+			create (file, Name => S);
+			put_line (file, "SOURCE");
+			close (file);
+			if Overwrite then
+				create (file, Name => T);
+				put_line (file, "TARGET");
+				close (file);
+			end if;
 		end;
-		pragma Assert (End_Of_File (file));
-		close (file);
-	end;
-	ada.directories.Delete_File (T);
+		ada.directories.replace_file (Source_Name => S, Target_Name => T);
+		pragma Assert (Ada.Directories.Exists (T));
+		pragma Assert (not Ada.Directories.Exists (S));
+		declare
+			use ada.text_io;
+			file : file_type;
+		begin
+			open (file, In_File, Name => T);
+			declare
+				Line : constant String := Get_Line (file);
+			begin
+				pragma Assert (Line = "SOURCE");
+				null;
+			end;
+			pragma Assert (End_Of_File (file));
+			close (file);
+		end;
+		ada.directories.Delete_File (T);
+		pragma Assert (not Ada.Directories.Exists (S));
+		pragma Assert (not Ada.Directories.Exists (T));
+	end loop;
 	-- finished
 	pragma Debug (Ada.Debug.Put ("OK"));
 end directories_replace_file;
