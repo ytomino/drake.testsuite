d7406c6d539bd876896c130da512900a5a343b0f
ytomino <demoonlit@panathenaia.halfmoon.jp>
2019-08-14 16:08:35 +0900

    Add some tests for plus or minus 2 seconds around the leap second

--- calendar_leapsecond.adb
+++ calendar_leapsecond.adb
@@ -7,6 +7,63 @@ with System.Native_Calendar;
 procedure calendar_leapsecond is
 	use type Ada.Calendar.Time;
 	use type Ada.Calendar.Time_Zones.Time_Offset;
+	procedure Try_UTC_Time_Offset (
+		T : Ada.Calendar.Time;
+		TZ_Offset : Ada.Calendar.Time_Zones.Time_Offset) is
+	begin
+		pragma Assert (Ada.Calendar.Time_Zones.UTC_Time_Offset (T) = TZ_Offset);
+		null;
+	end Try_UTC_Time_Offset;
+	procedure Try_Seconds (
+		T : Ada.Calendar.Time;
+		TZ_Offset : Ada.Calendar.Time_Zones.Time_Offset;
+		GMT_Seconds : Ada.Calendar.Day_Duration;
+		Local_Seconds : Ada.Calendar.Day_Duration)
+	is
+		Year : Ada.Calendar.Year_Number;
+		Month : Ada.Calendar.Month_Number;
+		Day : Ada.Calendar.Day_Number;
+		Seconds : Ada.Calendar.Day_Duration;
+		Leap_Second : Boolean;
+	begin
+		Ada.Calendar.Formatting.Split (T,
+			Year => Year, Month => Month, Day => Day, Seconds => Seconds,
+			Leap_Second => Leap_Second, Time_Zone => 0);
+		pragma Assert (Seconds = GMT_Seconds);
+		pragma Assert (
+			Ada.Calendar.Formatting.Time_Of (
+				Year => Year, Month => Month, Day => Day, Seconds => Seconds,
+				Leap_Second => Leap_Second, Time_Zone => 0) =
+			T);
+		Ada.Calendar.Formatting.Split (T,
+			Year => Year, Month => Month, Day => Day, Seconds => Seconds,
+			Leap_Second => Leap_Second, Time_Zone => TZ_Offset);
+		pragma Assert (Seconds = Local_Seconds);
+		pragma Assert (
+			Ada.Calendar.Formatting.Time_Of (
+				Year => Year, Month => Month, Day => Day, Seconds => Seconds,
+				Leap_Second => Leap_Second, Time_Zone => TZ_Offset) =
+			T);
+	end Try_Seconds;
+	procedure Try_Image (
+		T : Ada.Calendar.Time;
+		TZ_Offset : Ada.Calendar.Time_Zones.Time_Offset;
+		Second : Ada.Calendar.Formatting.Second_Number'Base)
+	is
+		GMT_Image : constant String :=
+			Ada.Calendar.Formatting.Image (T, Time_Zone => 0);
+		Local_Image : constant String :=
+			Ada.Calendar.Formatting.Image (T, Time_Zone => TZ_Offset);
+	begin
+		pragma Assert (
+			Ada.Calendar.Formatting.Value (GMT_Image, Time_Zone => 0) = T);
+		pragma Assert (
+			Ada.Calendar.Formatting.Value (Local_Image, Time_Zone => TZ_Offset) =
+			T);
+		pragma Assert (Integer'Value (GMT_Image (18 .. 19)) = Second);
+		pragma Assert (Integer'Value (Local_Image (18 .. 19)) = Second);
+		null;
+	end Try_Image;
 	procedure Try_TZ (
 		TZ_Name : String;
 		TZ_Offset : Ada.Calendar.Time_Zones.Time_Offset;
@@ -15,21 +72,35 @@ procedure calendar_leapsecond is
 		Local_Day : Ada.Calendar.Day_Number;
 		Local_Seconds : Ada.Calendar.Day_Duration;
 		Local_Hour : Ada.Calendar.Formatting.Hour_Number;
-		Local_Minute : Ada.Calendar.Formatting.Minute_Number;
-		Local_Image : String) is
+		Local_Minute : Ada.Calendar.Formatting.Minute_Number) is
 	begin
 		if Ada.Directories.Exists (TZ_Name) then
 			Ada.Debug.Put (TZ_Name);
 			Ada.Environment_Variables.Set ("TZ", TZ_Name);
 			System.Native_Calendar.Initialize_Time_Zones; -- force to reinitialize
+			declare -- -2
+				The_Prev_2_Time : constant Ada.Calendar.Time :=
+					Ada.Calendar.Time_Of (1998, 12, 31, 86398.0);
+			begin
+				Try_UTC_Time_Offset (The_Prev_2_Time, TZ_Offset);
+				Try_Seconds (The_Prev_2_Time, TZ_Offset,
+					86398.0, Local_Seconds - 1.0);
+				Try_Image (The_Prev_2_Time, TZ_Offset, 58);
+			end;
+			declare -- -1
+				The_Prev_Time : constant Ada.Calendar.Time :=
+					Ada.Calendar.Time_Of (1998, 12, 31, 86399.0);
+			begin
+				Try_UTC_Time_Offset (The_Prev_Time, TZ_Offset);
+				Try_Seconds (The_Prev_Time, TZ_Offset, 86399.0, Local_Seconds);
+				Try_Image (The_Prev_Time, TZ_Offset, 59);
+			end;
 			declare
 				The_LS_Time : constant Ada.Calendar.Time :=
 					Ada.Calendar.Time_Of (1999, 1, 1, 0.0) - 1.0;
-				GMT_Image : constant String := "1998-12-31 23:59:60";
 			begin
-				pragma Assert (
-					Ada.Calendar.Time_Zones.UTC_Time_Offset (The_LS_Time) =
-					TZ_Offset);
+				Try_UTC_Time_Offset (The_LS_Time, TZ_Offset);
+				Try_Seconds (The_LS_Time, TZ_Offset, 86399.0, Local_Seconds);
 				-- split/time_of
 				declare
 					Year : Ada.Calendar.Year_Number;
@@ -152,21 +223,23 @@ procedure calendar_leapsecond is
 							Leap_Second => True, Time_Zone => TZ_Offset) =
 						The_LS_Time);
 				end;
-				-- image/value
-				pragma Assert (
-					Ada.Calendar.Formatting.Image (The_LS_Time, Time_Zone => 0) =
-					GMT_Image);
-				pragma Assert (
-					Ada.Calendar.Formatting.Image (The_LS_Time,
-						Time_Zone => TZ_Offset) =
-					Local_Image);
-				pragma Assert (
-					Ada.Calendar.Formatting.Value (GMT_Image, Time_Zone => 0) =
-					The_LS_Time);
-				pragma Assert (
-					Ada.Calendar.Formatting.Value (Local_Image,
-						Time_Zone => TZ_Offset) =
-					The_LS_Time);
+				Try_Image (The_LS_Time, TZ_Offset, 60);
+			end;
+			declare -- +1
+				The_Next_Time : constant Ada.Calendar.Time :=
+					Ada.Calendar.Time_Of (1999, 1, 1, 0.0);
+			begin
+				Try_UTC_Time_Offset (The_Next_Time, TZ_Offset);
+				Try_Seconds (The_Next_Time, TZ_Offset, 0.0, Local_Seconds + 1.0);
+				Try_Image (The_Next_Time, TZ_Offset, 0);
+			end;
+			declare -- +2
+				The_Next_2_Time : constant Ada.Calendar.Time :=
+					Ada.Calendar.Time_Of (1999, 1, 1, 1.0);
+			begin
+				Try_UTC_Time_Offset (The_Next_2_Time, TZ_Offset);
+				Try_Seconds (The_Next_2_Time, TZ_Offset, 1.0, Local_Seconds + 2.0);
+				Try_Image (The_Next_2_Time, TZ_Offset, 1);
 			end;
 		else
 			Ada.Debug.Put (TZ_Name & " is not found.");
@@ -175,19 +248,15 @@ procedure calendar_leapsecond is
 begin
 	Try_TZ (
 		"/usr/share/zoneinfo/right/Japan", 9 * 60,
-		1999, 1, 1, 32399.0, 8, 59,
-		"1999-01-01 08:59:60");
+		1999, 1, 1, 32399.0, 8, 59);
 	Try_TZ (
 		"/usr/share/zoneinfo/right/Europe/Paris", 1 * 60,
-		1999, 1, 1, 3599.0, 0, 59,
-		"1999-01-01 00:59:60");
+		1999, 1, 1, 3599.0, 0, 59);
 	Try_TZ (
 		"/usr/share/zoneinfo/right/Etc/GMT+1", -1 * 60,
-		1998, 12, 31, 82799.0, 22, 59,
-		"1998-12-31 22:59:60");
+		1998, 12, 31, 82799.0, 22, 59);
 	Try_TZ (
 		"/usr/share/zoneinfo/right/US/Hawaii", -10 * 60,
-		1998, 12, 31, 50399.0, 13, 59,
-		"1998-12-31 13:59:60");
+		1998, 12, 31, 50399.0, 13, 59);
 	pragma Debug (Ada.Debug.Put ("OK"));
 end calendar_leapsecond;
